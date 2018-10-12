e53c8f167c0d3a04fed470d1a1ff1b2050e0fd1d
Daniel Hampikian <danielhampikian@gmail.com>
2018-10-12 08:44:50 -0600

    added networking for projectile and menu

---
+++ Networking/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MenuManager : NetworkBehaviour
+{
+    public Dropdown playerSelect;
+    public GameObject[] playerArray;
+
+    public void SelectPlayer()
+    {
+        Debug.Log("Player select value: " + playerSelect.value);
+
+        NetworkManager.singleton.playerPrefab = playerArray[playerSelect.value];
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+}
---
+++ Networking/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 5;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
