9e4b1bc96cd19e4dc612a8ebf83927d5786b6e04
Daniel Hampikian <danielhampikian@gmail.com>
2018-10-12 15:00:39 -0600

    added working custom network as a complete project and a unity package, tested on mac only

---
+++ Networking/ProjectileNetwork/Assets/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MenuManager : NetworkBehaviour
+{
+    public Dropdown playerSelect;
+    public GameObject[] playerArray;
+    public GameObject serverMenu;
+    public Text serverIP;
+
+    public void SelectPlayer()
+    {
+        NetworkManager.singleton.playerPrefab = playerArray[playerSelect.value];
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.singleton.StartHost();
+        serverMenu.SetActive(false);
+    }
+
+    public void StartClient()
+    {
+        getIP();
+        NetworkManager.singleton.StartClient();
+        serverMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            serverMenu.SetActive(true);
+        }
+    }
+    public void getIP(){
+        if (serverIP.text.Length > 0 && serverIP.text!= null){
+            NetworkManager.singleton.networkAddress = serverIP.text;
+        }
+    }
+
+}
---
+++ Networking/ProjectileNetwork/Assets/SetupLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(!isLocalPlayer){
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+            GetComponent<FireProjectile>().enabled = false;
+            GetComponentInChildren<Camera>().enabled = false;
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
